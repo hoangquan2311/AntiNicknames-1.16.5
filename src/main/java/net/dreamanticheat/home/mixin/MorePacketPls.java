01af451f8e021b7cc25bd9b9ecbc537908eda9c8
Hoang Quan <89526802+hoangquan2311@users.noreply.github.com>
2023-06-28 20:59:54 +0700

    Update MorePacketPls.java

--- src/main/java/net/dreamanticheat/home/mixin/MorePacketPls.java
+++ src/main/java/net/dreamanticheat/home/mixin/MorePacketPls.java
@@ -3,14 +3,6 @@ package net.dreamanticheat.home.mixin;
 import com.mojang.authlib.GameProfile;
 import net.dreamanticheat.home.AntiNickname;
 import net.dreamanticheat.home.TextColor;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.minecraft.network.packet.c2s.play.PlayerInteractBlockC2SPacket;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.network.ServerPlayNetworkHandler;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.MutableText;
-import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,14 +11,12 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
-@Mixin(ServerPlayNetworkHandler.class)
+@Mixin(ServerNetwork.class)
 public abstract class MorePacketPls {
-	@Shadow @Final private MinecraftServer server = AntiNickname.MC.getServer();
 
-	@Inject(method = "onPlayerInteractBlock", at = @At("HEAD"), cancellable = true)
-	private void onPlayerInteractBlock(PlayerInteractBlockC2SPacket packet, CallbackInfo info) {
-		ServerPlayerEntity player = ((ServerPlayNetworkHandler) (Object) this).player;
-		if (packet.getBlockHitResult().getBlockPos().getY() == 255) {
+	@Inject(method = "onInteract", at = @At("HEAD"), cancellable = true)
+	private void onPlayerInteractBlock(PlayerInteractBlockC2SPacket packet) {
+		if (packet.getBlockHitResult().getBlockPos()) {
 			MutableText msg = TextColor.text("Cấm sử dụng NoComCrash.", Formatting.RED);
 			MutableText suffix = TextColor.text("\n\nAnti-Hack by Dream_Da_Vang",Formatting.GRAY);
 			msg.append(suffix);
@@ -35,11 +25,9 @@ public abstract class MorePacketPls {
 			for (ServerPlayerEntity entity : server.getPlayerManager().getPlayerList()) {
 				GameProfile playerProfile = player.getGameProfile();
 				if (server.isHost(playerProfile)) {
-					player.sendMessage(hostMsg, false);
 					break; // Chỉ gửi tin nhắn cho người chơi host đầu tiên tìm thấy (nếu có nhiều người chơi host)
 				}
 			}
-			info.cancel();
 		}
 	}
 }
