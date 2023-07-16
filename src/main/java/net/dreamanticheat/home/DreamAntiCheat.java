708e6bc0ce79549db18bf5ab84db93c88b60ca5c
Hoang Quan <89526802+hoangquan2311@users.noreply.github.com>
2023-07-16 23:24:17 +0700

    Update DreamAntiCheat.java

--- src/main/java/net/dreamanticheat/home/DreamAntiCheat.java
+++ src/main/java/net/dreamanticheat/home/DreamAntiCheat.java
@@ -6,7 +6,5 @@ public class DreamAntiCheat implements ModInitializer {
     public static final String MOD_ID = "anti-cheat";
     @Override
     public void onInitialize() {
-        AntiNickname.initialize();
-        AntiLogAcc.initialize();
     }
 }
